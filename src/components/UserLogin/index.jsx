d429e0dba1cd8d8172b4f280ec701186d0c87e29
SWQXDBA <983110853@qq.com>
2021-11-21 10:33:15 +0800

    123

--- src/components/UserLogin/index.jsx
+++ src/components/UserLogin/index.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import axios from "axios";
 import {urls} from "../../configs/urls";
-
+import './index.css'
 
 
 const {Title} = Typography;
@@ -45,55 +45,55 @@ const {Title} = Typography;
     render() {
         return (
             <>
+                    <Row justify="center" align = "middle" style = {{height:'600px'}}>
+                        <Col span={6} style={{border: '1px solid black', padding: '15px'}}>
+                            <Title>停车场系统-用户登录</Title>
+                            <Form
+                                name="normal_login"
+                                className="login-form"
+                                initialValues={{remember: true}}
 
-
-                        <Row justify="center" align = "middle" style = {{height:'600px'}}>
-                            <Col span={6} style={{border: '1px solid black', padding: '15px'}}>
-                                <Title>停车场系统-用户登录</Title>
-                                <Form
-                                    name="normal_login"
-                                    className="login-form"
-                                    initialValues={{remember: true}}
-
+                            >
+                                <Form.Item
+                                    name="username"
+                                    rules={[{required: true, message: 'Please input your Username!'}]}
                                 >
-                                    <Form.Item
-                                        name="username"
-                                        rules={[{required: true, message: 'Please input your Username!'}]}
-                                    >
-                                        <Input prefix={<UserOutlined className="site-form-item-icon"/>}
-                                               placeholder="请输入用户名" onChange={this.onUserNameChange}/>
-                                    </Form.Item>
-                                    <Form.Item
-                                        name="password"
-                                        rules={[{required: true, message: 'Please input your Password!'}]}
-                                    >
-                                        <Input
-                                            prefix={<LockOutlined className="site-form-item-icon"/>}
-                                            type="password"
-                                            placeholder="请输入密码" onChange={this.onPassWordChange}
-                                        />
-                                    </Form.Item>
-                                    <Form.Item>
-                                        <Form.Item name="remember" valuePropName="checked" noStyle>
-                                            <Checkbox>记住密码</Checkbox>
-                                        </Form.Item>
-
-                                        <a className="login-form-forgot" href="">
-                                            忘记密码？点击找回
-                                        </a>
+                                    <Input prefix={<UserOutlined className="site-form-item-icon"/>}
+                                           placeholder="请输入用户名" onChange={this.onUserNameChange}/>
+                                </Form.Item>
+                                <Form.Item
+                                    name="password"
+                                    rules={[{required: true, message: 'Please input your Password!'}]}
+                                >
+                                    <Input
+                                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                                        type="password"
+                                        placeholder="请输入密码" onChange={this.onPassWordChange}
+                                    />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                                        <Checkbox>记住密码</Checkbox>
                                     </Form.Item>
 
+                                    <a className="login-form-forgot" href="">
+                                        忘记密码？点击找回
+                                    </a>
+                                </Form.Item>
 
-                                    <Form.Item>
-                                        <Button type="primary" htmlType="submit" className="login-form-button"
-                                                onClick={this.onFill}>
-                                            登录
-                                        </Button>
-                                        &nbsp; 或者 &nbsp;  <a href="">注册新账户</a>
-                                    </Form.Item>
-                                </Form>
-                            </Col>
-                        </Row>
+
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" className="login-form-button"
+                                            onClick={this.onFill}>
+                                        登录
+                                    </Button>
+                                    &nbsp; 或者 &nbsp;  <a href="">注册新账户</a>
+                                </Form.Item>
+                            </Form>
+                        </Col>
+                    </Row>
+
+
 
 
 
