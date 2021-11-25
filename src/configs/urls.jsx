bf16cc459a2e7607ca1515f0ac5ea33d4f203348
SWQXDBA2 <SWQXDBA2>
2021-11-25 23:23:23 +0800

    1

--- src/configs/urls.jsx
+++ src/configs/urls.jsx
@@ -1,4 +1,4 @@
-export const urls = {
+export const urls2 = {
     userLoginUrl :'http://localhost:3000/user/login',
     userRegisterUrl :'http://localhost:3000/user/register',
     userGetOptionLogUrl:"http://localhost:3000/user/getOptionLog",
@@ -11,5 +11,20 @@ export const urls = {
     adminGetAllRentApplyUrl:'http://localhost:3000/admin/getAllRentApply',
     adminRefuseApplyUrl:'http://localhost:3000/admin/refuse',
     adminGetAllOptionLogUrl:'http://localhost:3000/admin/getAllOptionLog'
+
+}
+export const urls = {
+    userLoginUrl :'http://localhost:8080/api/user/login',
+    userRegisterUrl :'http://localhost:8080/api/user/register',
+    userGetOptionLogUrl:"http://localhost:8080/api/user/getOptionLog",
+    getAllParksUrl:'http://localhost:8080/api/parkingSpace/getAll',
+    getPrivateParkingSpaceUrl:'http://localhost:8080/api/parkingSpace/getByUser',
+    rentApply:'http://localhost:8080/api/user/rent',
+    adminLoginUrl:'http://localhost:8080/api/admin/login',
+    adminAddAdminUrl:'http://localhost:8080/api/admin/addAdmin',
+    adminAdmitRentApplyUrl:'http://localhost:8080/api/admin/admit',
+    adminGetAllRentApplyUrl:'http://localhost:8080/api/admin/getAllRentApply',
+    adminRefuseApplyUrl:'http://localhost:8080/api/admin/refuse',
+    adminGetAllOptionLogUrl:'http://localhost:8080/api/admin/getAllOptionLog'
 
 }
