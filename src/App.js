515d828fcab1a3320e413d8c3a0ecfd0d0916844
SWQXDBA2 <SWQXDBA2>
2021-11-25 21:18:47 +0800

    1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 function App() {
-console.log(routers)
   return (
     <div className="App">
       <Router>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -24,7 +24,6 @@ module.exports = function (app) {
         createProxyMiddleware({ //遇见/api1前缀的请求，就会触发该代理配置
             target: 'http://localhost:8080', //请求转发给谁
             changeOrigin: true, //控制服务器收到的请求头中Host的值
-
         })
     )
 
