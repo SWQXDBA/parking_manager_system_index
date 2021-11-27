bf5ecc4b6762d794ec63a152ef5ca213a62af986
SWQXDBA <983110853@qq.com>
2021-11-27 21:52:22 +0800

    1

--- src/configs/urls.jsx
+++ src/configs/urls.jsx
@@ -1,29 +1,30 @@
 export const urls2 = {
-    userLoginUrl :'http://localhost:3000/user/login',
-    userRegisterUrl :'http://localhost:3000/user/register',
-    userGetOptionLogUrl:"http://localhost:3000/user/getOptionLog",
-    getAllParksUrl:'http://localhost:3000/parkingSpace/getAll',
-    getPrivateParkingSpaceUrl:'http://localhost:3000/parkingSpace/getByUser',
-    rentApply:'http://localhost:3000/user/rent',
-    adminLoginUrl:'http://localhost:3000/admin/login',
-    adminAddAdminUrl:'http://localhost:3000/admin/addAdmin',
-    adminAdmitRentApplyUrl:'http://localhost:3000/admin/admit',
-    adminGetAllRentApplyUrl:'http://localhost:3000/admin/getAllRentApply',
-    adminRefuseApplyUrl:'http://localhost:3000/admin/refuse',
-    adminGetAllOptionLogUrl:'http://localhost:3000/admin/getAllOptionLog'
+    userLoginUrl :'http://43.248.186.196:3000/user/login',
+    userRegisterUrl :'http://43.248.186.196:3000/user/register',
+    userGetOptionLogUrl:"http://43.248.186.196:3000/user/getOptionLog",
+    getAllParksUrl:'http://43.248.186.196:3000/parkingSpace/getAll',
+    getPrivateParkingSpaceUrl:'http://43.248.186.196:3000/parkingSpace/getByUser',
+    rentApply:'http://43.248.186.196:3000/user/rent',
+    adminLoginUrl:'http://43.248.186.196:3000/admin/login',
+    adminAddAdminUrl:'http://43.248.186.196:3000/admin/addAdmin',
+    adminAdmitRentApplyUrl:'http://43.248.186.196:3000/admin/admit',
+    adminGetAllRentApplyUrl:'http://43.248.186.196:3000/admin/getAllRentApply',
+    adminRefuseApplyUrl:'http://43.248.186.196:3000/admin/refuse',
+    adminGetAllOptionLogUrl:'http://43.248.186.196:3000/admin/getAllOptionLog'
 }
 export const urls = {
-    userLoginUrl :'http://localhost:8998/api/user/login',
-    userRegisterUrl :'http://localhost:8998/api/user/register',
-    userGetOptionLogUrl:"http://localhost:8998/api/user/getOptionLog",
-    getAllParksUrl:'http://localhost:8998/api/parkingSpace/getAll',
-    getPrivateParkingSpaceUrl:'http://localhost:8998/api/parkingSpace/getByUser',
-    rentApply:'http://localhost:8998/api/user/rent',
-    adminLoginUrl:'http://localhost:8998/api/admin/login',
-    adminAddAdminUrl:'http://localhost:8998/api/admin/addAdmin',
-    adminAdmitRentApplyUrl:'http://localhost:8998/api/admin/admit',
-    adminGetAllRentApplyUrl:'http://localhost:8998/api/admin/getAllRentApply',
-    adminRefuseApplyUrl:'http://localhost:8998/api/admin/refuse',
-    adminGetAllOptionLogUrl:'http://localhost:8998/api/admin/getAllOptionLog'
+    userLoginUrl :'http://43.248.186.196:8093/api/user/login',
+    userRegisterUrl :'http://43.248.186.196:8093/api/user/register',
+    userGetOptionLogUrl:"http://43.248.186.196:8093/api/user/getOptionLog",
+    getAllParksUrl:'http://43.248.186.196:8093/api/parkingSpace/getAll',
+    getPrivateParkingSpaceUrl:'http://43.248.186.196:8093/api/parkingSpace/getByUser',
+    rentApply:'http://43.248.186.196:8093/api/user/rent',
+    adminLoginUrl:'http://43.248.186.196:8093/api/admin/login',
+    adminAddAdminUrl:'http://43.248.186.196:8093/api/admin/addAdmin',
+    adminAdmitRentApplyUrl:'http://43.248.186.196:8093/api/admin/admit',
+    adminGetAllRentApplyUrl:'http://43.248.186.196:8093/api/admin/getAllRentApply',
+    adminRefuseApplyUrl:'http://43.248.186.196:8093/api/admin/refuse',
+    adminGetAllOptionLogUrl:'http://43.248.186.196:8093/api/admin/getAllOptionLog'
 
 }
+//43.248.18 6.196
