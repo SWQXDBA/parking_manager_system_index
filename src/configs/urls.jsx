88a387ab4090f7176ea7b29b90c91986c37748e4
98311 <983110853@qq.com>
2022-06-13 09:05:42 +0800

    update config

--- src/configs/urls.jsx
+++ src/configs/urls.jsx
@@ -1,29 +1,17 @@
-export const urls2 = {
-    userLoginUrl :'http://43.248.186.196:3000/user/login',
-    userRegisterUrl :'http://43.248.186.196:3000/user/register',
-    userGetOptionLogUrl:"http://43.248.186.196:3000/user/getOptionLog",
-    getAllParksUrl:'http://43.248.186.196:3000/parkingSpace/getAll',
-    getPrivateParkingSpaceUrl:'http://43.248.186.196:3000/parkingSpace/getByUser',
-    rentApply:'http://43.248.186.196:3000/user/rent',
-    adminLoginUrl:'http://43.248.186.196:3000/admin/login',
-    adminAddAdminUrl:'http://43.248.186.196:3000/admin/addAdmin',
-    adminAdmitRentApplyUrl:'http://43.248.186.196:3000/admin/admit',
-    adminGetAllRentApplyUrl:'http://43.248.186.196:3000/admin/getAllRentApply',
-    adminRefuseApplyUrl:'http://43.248.186.196:3000/admin/refuse',
-    adminGetAllOptionLogUrl:'http://43.248.186.196:3000/admin/getAllOptionLog'
-}
+const host = "43.248.186.196";
+const port = '8088'
+
 export const urls = {
-    userLoginUrl :'http://43.248.186.196:8088/api/user/login',
-    userRegisterUrl :'http://43.248.186.196:8088/api/user/register',
-    userGetOptionLogUrl:"http://43.248.186.196:8088/api/user/getOptionLog",
-    getAllParksUrl:'http://43.248.186.196:8088/api/parkingSpace/getAll',
-    getPrivateParkingSpaceUrl:'http://43.248.186.196:8088/api/parkingSpace/getByUser',
-    rentApply:'http://43.248.186.196:8088/api/user/rent',
-    adminLoginUrl:'http://43.248.186.196:8088/api/admin/login',
-    adminAddAdminUrl:'http://43.248.186.196:8088/api/admin/addAdmin',
-    adminAdmitRentApplyUrl:'http://43.248.186.196:8088/api/admin/admit',
-    adminGetAllRentApplyUrl:'http://43.248.186.196:8088/api/admin/getAllRentApply',
-    adminRefuseApplyUrl:'http://43.248.186.196:8088/api/admin/refuse',
-    adminGetAllOptionLogUrl:'http://43.248.186.196:8088/api/admin/getAllOptionLog'
+    userLoginUrl :'http://'+host+':'+port+'/api/user/login',
+    userRegisterUrl :'http://'+host+':'+port+'/api/user/register',
+    userGetOptionLogUrl:'http://'+host+':'+port+'/api/user/getOptionLog',
+    getAllParksUrl:'http://'+host+':'+port+'/api/parkingSpace/getAll',
+    getPrivateParkingSpaceUrl:'http://'+host+':'+port+'/api/parkingSpace/getByUser',
+    rentApply:'http://'+host+':'+port+'/api/user/rent',
+    adminLoginUrl:'http://'+host+':'+port+'/api/admin/login',
+    adminAddAdminUrl:'http://'+host+':'+port+'/api/admin/addAdmin',
+    adminAdmitRentApplyUrl:'http://'+host+':'+port+'/api/admin/admit',
+    adminGetAllRentApplyUrl:'http://'+host+':'+port+'/api/admin/getAllRentApply',
+    adminRefuseApplyUrl:'http://'+host+':'+port+'/api/admin/refuse',
+    adminGetAllOptionLogUrl:'http://'+host+':'+port+'/api/admin/getAllOptionLog'
 }
-//43.248.186.196:8088
